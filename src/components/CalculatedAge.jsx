80676fa14223d3aee27f77924705f342b61e51eb
pkthunder87 <pkthunder87@gmail.com>
2023-11-22 19:26:42 -0800

    Feat: Add initial content to CalculatedAge.jsx

--- src/components/CalculatedAge.jsx
+++ src/components/CalculatedAge.jsx
@@ -1,10 +1,40 @@
+import styled from "styled-components";
+
 function CalculatedAge() {
+  const StyledCalculatedAge = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-left: 3.6rem;
+    letter-spacing: -1px;
+  `;
+
+  const Date = styled.h2`
+    font-size: 10.3rem;
+    font-weight: 800;
+    display: flex;
+    gap: 1.2rem;
+    align-items: center;
+    margin-top: -3.2rem;
+    margin-bottom: -1.1rem;
+  `;
+
+  const Age = styled.p`
+    color: var(--color-purple);
+    font-size: 10rem;
+  `;
+
   return (
-    <div>
-      <div>years</div>
-      <div>months</div>
-      <div>days</div>
-    </div>
+    <StyledCalculatedAge>
+      <Date>
+        <Age>- -</Age>years
+      </Date>
+      <Date>
+        <Age>- -</Age>months
+      </Date>
+      <Date>
+        <Age>- -</Age>days
+      </Date>
+    </StyledCalculatedAge>
   );
 }
 
