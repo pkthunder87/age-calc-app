72c309af8da4ed702ed05484990826bfe32c0043
pkthunder87 <pkthunder87@gmail.com>
2023-11-27 11:08:58 -0800

    Feat: Finish non active state of static site

--- src/components/BirthdayForm.jsx
+++ src/components/BirthdayForm.jsx
@@ -2,11 +2,19 @@ import styled from "styled-components";
 import { useForm } from "react-hook-form";
 
 function BirthdayForm() {
+  function onSubmit(data) {
+    console.log(data.day, data.month, data.year);
+  }
+
+  const { register, handleSubmit } = useForm();
+
   const StyledBirthdayForm = styled.form`
     display: flex;
+    column-gap: 3.2rem;
+
     height: 50%;
     width: 80%;
-    column-gap: 3.2rem;
+
     margin-top: 4rem;
     margin-left: 3.6rem;
 
@@ -19,11 +27,13 @@ function BirthdayForm() {
   const StyledInput = styled.div`
     display: flex;
     flex-direction: column;
+
     font-style: normal;
     color: var(--color-smokey-grey);
 
     label {
       margin-top: -0.2rem;
+
       font-size: 1.3rem;
       letter-spacing: 0.33em;
       font-weight: 700;
@@ -32,33 +42,52 @@ function BirthdayForm() {
 
     input {
       margin-top: 0.8rem;
+      display: flex;
+
       height: 7.2rem;
       width: 16rem;
+
       border-radius: 8px;
       border: 1.2px solid var(--color-light-grey);
       padding-left: 2.3rem;
+      padding-top: 1.5rem;
 
       &::placeholder {
         font-weight: 800;
         font-size: 3.2rem;
-        color: hsl(0, 1%, 14%);
       }
     }
   `;
 
-  function onSubmit(data) {
-    console.log(data.day, data.month, data.year);
-  }
-
-  const { register, handleSubmit } = useForm();
-
   const StyledButton = styled.button`
-    height: 4rem;
-    width: 8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 9.6rem;
+    width: 9.6rem;
+
+    cursor: pointer;
+    border: none;
+    border-radius: 50%;
+    background-color: var(--color-purple);
+
+    position: absolute;
+    right: 3.6rem;
+    bottom: 1.2rem;
+    z-index: 2;
   `;
 
   const BirthdayLayout = styled.div`
-    /* background-color: red; */
+    position: relative;
+  `;
+
+  const SolidLine = styled.div`
+    margin-top: 2.2rem;
+    margin-left: 3.6rem;
+    width: 80%;
+
+    border: 1px solid var(--color-off-white);
   `;
 
   return (
@@ -90,8 +119,13 @@ function BirthdayForm() {
         </StyledInput>
       </StyledBirthdayForm>
 
+      <SolidLine></SolidLine>
       <StyledButton type="submit" form="inputDay" value="Submit">
-        Submit
+        <img
+          className=" "
+          src="./icon-arrow.svg"
+          alt="arrow pointing downwards"
+        />
       </StyledButton>
     </BirthdayLayout>
   );
